d34663f0d5b077bb39f13eab0b417d4ec4cf3dcf
Kosmo_10 <Kosmo_10@DESKTOP-PTM1T3K>
2021-02-16 12:42:34 +0900

    add Practices

--- dev_java80/src/com/base/Practice3.java
+++ dev_java80/src/com/base/Practice3.java
@@ -5,7 +5,7 @@ public class Practice3 {
 	public static void main(String[] args) {
 		//String #name = "Jane Doe";
 		int $age = 24;
-		double _height = 123.5;
+		double height = 123.5;
 		//double ~temp = 37.5;
 		
 	}
